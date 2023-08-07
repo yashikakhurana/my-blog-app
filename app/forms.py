3f38cdc80750e88cb5e676674f094d1fd9d244a9
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-07 14:18:16 -0700

    feat(blog): placeholder comments

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,12 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = {"content", "email", "name", "website"}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["content"].widget.attrs[
+            "placeholder"
+        ] = "Type your comment here...."
+        self.fields["name"].widget.attrs["placeholder"] = "Name"
+        self.fields["email"].widget.attrs["placeholder"] = "Email"
+        self.fields["website"].widget.attrs["placeholder"] = "Website"
