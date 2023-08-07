98b0062dee56c1ba4e2b242c54a9db78996645dc
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-07 13:06:24 -0700

    feat(blog): Render posts

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("post/<slug:slug>", views.post_page, name="post_page")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("post/<slug:slug>", views.post_page, name="post_page"),
+]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from app.models import Post
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "app/index.html", context=context)
 
 
 def post_page(request, slug):
