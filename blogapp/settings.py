3d54147bfa63cc36e4600318d2ece4420667668c
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-06 18:41:37 -0700

    feat(blog): Post model

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app.apps.AppConfig"
+    "app.apps.AppConfig",
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / "upload"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
