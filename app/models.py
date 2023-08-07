37ff3cec4e49e0d0a6ada4879700fa42a8767088
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-07 13:24:49 -0700

    feat(blog): Tags

--- app/models.py
+++ app/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -9,3 +24,4 @@ class Post(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="post")
