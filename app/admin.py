e6a4b4969916901f39dca2da6271fb2ca0b5822d
yashikakhurana <yashikakhuranayashika@gmail.com>
2023-08-09 16:27:27 -0700

    feat(blog): show profile image and username

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from app.models import Post, Tag, Comment, Subscribe
+from app.models import Post, Profile, Tag, Comment, Subscribe
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Subscribe)
+admin.site.register(Profile)
